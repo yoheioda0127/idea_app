5d40859759eee9b25cd6af52baa1677561644572
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-18 06:25:47 +0900

    マイグレーション実行

--- db/migrate/20210317210928_create_ideas.rb
+++ db/migrate/20210317210928_create_ideas.rb
@@ -2,6 +2,9 @@ class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
 
+      t.integer :category_id, null: false, foreign_key: true
+      t.text    :body
+
       t.timestamps
     end
   end

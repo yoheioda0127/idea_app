b8faa78c48d1a3784586a04718cc72c678cfe1a7
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-18 18:48:04 +0900

    新規投稿フォーム

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,30 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
   end
+
+  def new
+    @idea = Idea.new
+    @category = Category.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @category = Category.new(category_params)
+    if @idea.save && @category.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:category_id, :body)
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'ideas#index'
+  resources :ideas, only: [:index, :new, :create]
 end

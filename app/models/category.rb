33d66a5b3f71ba37196fb2baf314fddffeb39e37
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-21 07:18:21 +0900

    バリデーションの記載

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :idea
-end
+  
+  validates  :name,   presence: true, format:  { message: 'は必須内容です。' }, length: {maximum: 8, message: 'は8文字以内です。'}
+  end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,6 @@
 class Idea < ApplicationRecord
   has_one :category
+
+  validates :body,        ,presence: true, length:       {maximum: 300, message: 'は300文字以内です。'}
+  validates :category_id  ,presence: true, numericality: {message: 'の選択を行ってください！！'}
 end

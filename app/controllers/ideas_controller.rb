a2f6f028def6b1ab12c317ee63cdaeaaf655d396
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-21 15:45:38 +0900

    ビューの実装

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,25 +1,23 @@
 class IdeasController < ApplicationController
+  
   def index
     @ideas = Idea.all
-    # @category = Category.find(params[:category_id])
-    @category = Category.includes(:category_id)
+    @category = Category.all
   end
 
   def new
     @idea = Idea.new
+    @category = Category.new
   end
 
   def create
     @idea = Idea.new(idea_params)
+    @category = Category.new(category_params)
     if @idea.save
-      render action: :index
+      render :new
     else
-      render action: :index
+      render :new
     end
-  end
-
-  def show
-    @idea =Idea.find(params[:id])
   end
 
   def search
@@ -30,12 +28,21 @@ class IdeasController < ApplicationController
     end
   end
 
+  def destroy
+    idea = Idea.find(params[:id])
+    idea.destroy
+    redirect_to root_path
+  end
 
   
   private
 
   def idea_params
     params.require(:idea).permit(:category_id, :body)
+  end
+
+  def category_params
+    params.permit(:name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'ideas#index'
-  resources :ideas, only: [:index, :new, :create]
-  resources :categorys, only: [:new, :create]  
+  resources :ideas, only: [:index, :new, :create, :destroy]
+  resources :categorys, only: [:index, :new, :create]  
   
   resources :ideas do
     get :search, on: :collection

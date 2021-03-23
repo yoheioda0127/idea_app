114849bf130ec1fe8867f59b4181e32ae6c55e4a
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-24 06:11:02 +0900

    テストコード句読点追加

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -6,11 +6,11 @@ describe Idea do
 
   describe 'ideaの保存' do
     # 正常系↓
-    context "ideaが保存できる場合" do
-      it "bodyとcategory_idがあればideaは保存される（1）" do
+    context "ideaが保存できる場合。" do
+      it "bodyとcategory_idがあればideaは保存される（1）。" do
         expect(@idea).to be_valid
       end
-      it "bodyとcategory_idがあればideaは保存される（2）" do
+      it "bodyとcategory_idがあればideaは保存される（2）。" do
         idea = Idea.build(:idea)
         idea.valid?
       end
@@ -21,20 +21,20 @@ describe Idea do
       end
     end
     # 異常系↓
-    context "ideaが保存できない場合" do
-      it "bodyがないとideaは保存できない" do
+    context "ideaが保存できない場合。" do
+      it "bodyがないとideaは保存できない。" do
         idea = FactoryBot.build(:category)
         @idea.body = nil
         @idea.valid?
         expect(@idea.errors.full_messages).to include("bodyを入力してください。")
       end
-      it "category_idがないとideaは保存できない" do
+      it "category_idがないとideaは保存できない。" do
         idea = FactoryBot.build(:category)
         @idea.category_id = nil
         @idea.valid?
         expect(@idea.errors.full_messages).to include("category_idを選択してください。")
       end
-      it "nameが301文字だとideaは保存できない" do
+      it "nameが301文字だとideaは保存できない。" do
         idea = FactoryBot.build(:idea)
         @idea.body = 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
         @idea.valid?
